8c10b03917d983fc58d7e19f29918002ea8f4ac4
dangnhathuy <106954065+dangnhathuyy@users.noreply.github.com>
2022-06-06 16:09:04 +0700

    upload Project09

---
+++ 9.sound board/script.js
@@ -0,0 +1,21 @@
+const sounds = ['air-biscuit', 'alien-encounter']
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+  btn.innerText = sound
+  btn.addEventListener('click', () => {
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0
+  })
+}
